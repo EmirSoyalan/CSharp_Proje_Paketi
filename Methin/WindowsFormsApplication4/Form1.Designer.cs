531b5279192d759f2218991e93e1c5d85bc81cd3
EmirSoyalan <32599085+EmirSoyalan@users.noreply.github.com>
2020-04-05 15:34:48 +0300

    Project 3
    Methin

---
+++ Methin/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(126, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.button1.Location = new System.Drawing.Point(112, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBox2.Location = new System.Drawing.Point(112, 132);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(18, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Metni girin:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Aranacak karakter:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.Location = new System.Drawing.Point(12, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Arama sonucu:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.button2.Location = new System.Drawing.Point(400, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "faktoriyel bulma formu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(237, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 108);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.label5.Location = new System.Drawing.Point(236, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "karakterlerin yerleri";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F);
+            this.label6.Location = new System.Drawing.Point(12, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "(boşlukta girilebilir)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(237, 145);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(135, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Sadece baş harfleri ara";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(99, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(122, 80);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F);
+            this.button3.Location = new System.Drawing.Point(136, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 25);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(399, 257);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Methin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Methin/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ToolTip tip = new ToolTip();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Focus();
+            textBox2.MaxLength = 1;
+        }
+        int harf, bosluk, harff;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            harff=0;
+            label1.Text = ""; 
+            if (!string.IsNullOrWhiteSpace(richTextBox1.Text) || !string.IsNullOrEmpty(textBox2.Text))
+            {
+                if (string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    textBox2.Text = " ";
+
+                }
+                richTextBox1.Text.Trim();
+                harf = 0;
+                listBox1.Items.Clear();
+                if (textBox2.Text == " ")
+                {
+                    for (int i = 0; i < richTextBox1.Text.Length; i++)
+                    {
+                        if (richTextBox1.Text.Substring(i, 1) == " ")
+                        {
+                            harf++;
+                        }
+                    }
+                    if (harf == 0)
+                    {
+                        label1.Text = "Metinde hiç boşluk yok";
+                    }
+                    else
+                        label1.Text = "Metinde " + harf + " tane boşluk var";
+                }
+                else
+                {
+                    bosluk = 0;
+                    for (int i = 0; i < richTextBox1.Text.Length; i++)
+                    {
+                        char @char = char.Parse(richTextBox1.Text.Substring(i, 1));
+                        if (@char == ' ')
+                        {
+                            bosluk++;
+                            harf = 0;
+                        }
+                        else
+                        {
+                            harf++;
+                        }
+                        try
+                        {
+                            if (@char == char.Parse(textBox2.Text))
+                            {
+                                if (checkBox1.Checked)
+                                {
+                                    if (harf == 1)
+                                    {
+                                        listBox1.Items.Add((bosluk + 1) + ". kelimenin baş harfi ");
+                                        harff++;
+                                    }
+                                }
+                                else
+                                {
+                                    listBox1.Items.Add((bosluk + 1) + ". kelime, " + harf + ". harf");
+                                    harff++;
+                                }
+
+                            }
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                    if (checkBox1.Checked)
+                    {
+                        if (harff==0)
+                        {
+                            label1.Text = "Metinde " + textBox2.Text + " ile başlayan kelime yok";
+                        }
+                        else
+                        {
+                            label1.Text = "Metinde " + textBox2.Text + " ile başlayan " + harff + " kelime var";
+                        }
+                    }
+                    else
+                    {
+                        if (harff == 0)
+                        {
+                            label1.Text = "Metinde hiç '" + textBox2.Text + "' harfi yok";
+                        }
+                        else
+                        {
+                            label1.Text = "Metinde " + harff + " tane '" + textBox2.Text + "' harfi var";
+                        }
+                    }
+                   
+                }
+            }
+            else
+            {
+                MessageBox.Show("Gerekli alanları doldurunuz.", "DİKKAT", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            faktoriyel f = new faktoriyel();
+            f.Show();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("1");
+            }
+            if (string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                MessageBox.Show("2");
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("3");
+            }
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("4");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+    }
+}
+
